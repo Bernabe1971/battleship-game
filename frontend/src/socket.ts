61c743f6bff36edb0a64645d48a5728c64b50060
Bernabe1971 <130706900+Bernabe1971@users.noreply.github.com>
2026-05-18 18:01:15 -0600

    Enhance socket connection options fior reconection

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,4 +2,10 @@ import { io } from 'socket.io-client';
 
 const URL = window.location.origin;
 
-export const socket = io(URL, { autoConnect: false });
+export const socket = io(URL, {
+  autoConnect: false,
+  reconnection: true,
+  reconnectionAttempts: 20,
+  reconnectionDelay: 1000,
+  timeout: 10000,
+});
